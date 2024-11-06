a54c685e81d2991953e2276a341457b9857fb794
pe4enka <girfanov_kostia@mail.ru>
2024-11-06 07:19:52 +0500

    added creating elements from form

--- reactproject1/src/App.tsx
+++ reactproject1/src/App.tsx
@@ -1,32 +1,59 @@
 //import * as React from 'react';
+import React from 'react';
+import { createRoot } from 'react-dom/client'
 import './App.css'
 import BubbleSort from './sorts';
 
-function App() {
-    const components = [];
-    const bubble: BubbleSort = new BubbleSort(150, 300);
-    const maxHeightElement = bubble.maxNumber;
 
-    const elHeight: number = 600;
-    for (let i: number = 0; i < maxHeightElement; i++) {
-        components[i] = <span id={i.toString()} style={{
-            backgroundColor: "black",
-            height: `${(bubble.nums[i] / maxHeightElement * elHeight).toString()}px`,
-            width: "2px"
+function LoadNums() {
+    event!.preventDefault();
+    const components: React.ReactNode[] = [];
+    const maxElementHeight: number = 600;
 
-        }}></span>;
+    let relativeMaxElementHeight: number = Number((document.getElementById("maxElement")?.value));
+    const elementsCount: number = Number(document.getElementById("elementsCount")?.value);
+    const bubble: BubbleSort = new BubbleSort(elementsCount, relativeMaxElementHeight);
+    relativeMaxElementHeight = bubble.maxNumber;
+
+    const containerWidth: number = Number(document.getElementById("elementsContainer")?.clientWidth);
+    //const delay: number = Number(document.getElementById("delayBetweenSteps")?.value);
+    for (let i: number = 0; i < elementsCount; i++) {
+        components[i] =
+            <span
+                id={i.toString()}
+                style={{
+                    backgroundColor: "black",
+                    height: `${(bubble.nums[i] / relativeMaxElementHeight * maxElementHeight).toString()}px`,
+                    width: `${containerWidth / elementsCount}px`
+                }
+                }>
+            </span>
     }
+    createRoot(document.getElementById('elementsContainer')!).render(
+        components
+    )
+}
 
-  return (
-      <>
-        <div className="card">
-            {components}
-        </div>
-        <p className="read-the-docs">
-            Click on the Vite and React logos to learn more
-        </p>
-    </>
-  )
+function App() {
+    return (
+        <>
+            <div className="root">
+                <div id="controlPanel">
+                    <form>
+                        <label>Количество элементов</label>
+                        <input type="text" defaultValue="300" id="elementsCount"></input>
+                        <label>Максимальный элемент</label>
+                        <input type="text" defaultValue="10000" id="maxElement"></input>
+                        <label>Задержка между шагами(мс)</label>
+                        <input type="text" defaultValue="1" id="delayBetweenSteps"></input>
+                        <input type="submit" onClick={LoadNums}></input>
+                    </form>
+                </div>
+                <div className="card" id="elementsContainer">
+                </div>
+            </div>
+        </>
+    );
 }
 
 export default App
